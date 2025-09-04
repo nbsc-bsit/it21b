c0897c4cf81e88368134aa3fdd917fa5da2310f8
cliffamadeus <dev.clfm5t12@gmail.com>
2025-09-05 07:53:35 +0800

    Added menu items for factorial.js

--- factorial.js
+++ factorial.js
@@ -1,1 +1,6 @@
+console.log("---------------------------");
 console.log("Factorial Application");
+console.log("1. Say Hello");
+console.log("2. Factorial");
+console.log("3. Exit Application");
+console.log("---------------------------");
