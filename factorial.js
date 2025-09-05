2868626c63c74df6b6972b607c8d91b79aa7a196
cliffamadeus <dev.clfm5t12@gmail.com>
2025-09-05 08:04:23 +0800

    Added readline service and initial readline codeblock

--- factorial.js
+++ factorial.js
@@ -1,6 +1,18 @@
+//Console Input
+const readline=require("readline");
+
+const rl = readline.createInterface({
+    input:process.stdin,
+    output: process.stdout
+})
+
 console.log("---------------------------");
 console.log("Factorial Application");
 console.log("1. Say Hello");
 console.log("2. Factorial");
 console.log("3. Exit Application");
 console.log("---------------------------");
+
+rl.question("Enter your choice (1-3): ", choice =>{
+    console.log(choice);
+})
